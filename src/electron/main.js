16bd42934bde213222aa7d6d348c4ed35134ed7e
Justin White <juwhite@pavlovmedia.com>
2018-10-02 16:19:55 -0500

    Add close button

--- src/electron/main.js
+++ src/electron/main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 600})
+  mainWindow = new BrowserWindow({width: 1000, height: 600, frame: false, minWidth: 900, minHeight: 500})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
